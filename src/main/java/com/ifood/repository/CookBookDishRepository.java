59b30f5a4cff84b65c72c65b39dd11125896fa3e
quanghuy323 <quanghuy323@users.noreply.github.com>
2018-11-08 17:08:48 +0700

    Revert "Edit code : cookbook fix bug: account"
    This reverts commit 4dc016e

--- src/main/java/com/ifood/repository/CookBookDishRepository.java
+++ src/main/java/com/ifood/repository/CookBookDishRepository.java
@@ -1,21 +1,10 @@
 package com.ifood.repository;
 
 import com.ifood.domain.CookBookDishEntity;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.query.Param;
 
-import javax.transaction.Transactional;
 import java.util.List;
 
 public interface CookBookDishRepository extends CrudRepository<CookBookDishEntity, Integer> {
-    @Query("Select cd, d From CookBookDishEntity cd join DishEntity  d on cd.dishInCookbook.id = d.id" +
-            " Where cd.cookbookId = :cookbookId AND d.delete = false ")
-    List<CookBookDishEntity> findDishByCookbookId(@Param("cookbookId") String cookbookId);
-
-    @Transactional
-    @Modifying
-    @Query("Delete from CookBookDishEntity Where cookbookId = :cookbookId")
-    void deleteByCookbookId(@Param("cookbookId") String cookbookId);
+    List<CookBookDishEntity> findByCookbookId(String cookbookId);
 }
